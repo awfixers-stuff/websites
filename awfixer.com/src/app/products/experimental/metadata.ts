aafcc7a5ca2a0ae1b0e87f610350ad27696a2038
awfixer <git@awfixer.me>
2026-01-16 17:58:58 -0800

    feat: fix react for some reason (maybe because facebook sucks balls)

---
+++ awfixer.com/src/app/products/experimental/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Experimental Products | AWFixer",
+  description: "Cutting-edge experimental products and prototypes from AWFixer Labs.",
+};
